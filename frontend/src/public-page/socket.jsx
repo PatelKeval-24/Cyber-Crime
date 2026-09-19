52ffd8136a79998265118cb48bed9684809732b8
PatelKeval-24 <pkevu110@gmail.com>
2026-09-19 16:18:26 +0530

    socket.jsx file is corrected with the ulr

--- frontend/src/public-page/socket.jsx
+++ frontend/src/public-page/socket.jsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Use your AWS URL here if deployed, otherwise localhost
-const URL = `${import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'}`; // Use environment variable for backend URL
+const URL = `${import.meta.env.VITE_BACKEND_URL}`; // Use environment variable for backend URL
 
 export const socket = io(URL, {
     withCredentials: true,
